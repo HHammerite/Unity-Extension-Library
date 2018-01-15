fbe99a26f237916a16befc5852305043391bf30b
Harry Collin <harry.archie.collin@gmail.com>
2018-01-15 23:51:20 +0000

    Added the solution & two extension classes.
    LayerMask & Transform extension classes added.

---
+++ UnityExtensions/UnityExtensions/LayerMaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UnityExtensions
+{
+    public static class LayerMaskExtensions
+    {
+        /// <summary>
+        /// Checks if a layer is present in the mask
+        /// </summary>
+        /// <param name="layerMask"></param>
+        /// <param name="layer"></param>
+        /// <returns></returns>
+        public static bool IsInLayer(this LayerMask layerMask, int layer)
+        {
+            return layerMask == (layerMask | (1 << layer));
+        }
+    }
+}
---
+++ UnityExtensions/UnityExtensions/TransformExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UnityExtensions
+{
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Resets the transform's position & rotation respectively. 
+        /// </summary>
+        /// <param name="transform"></param>
+        public static void Reset(this Transform transform)
+        {
+            transform.rotation = Quaternion.identity;
+            transform.position = Vector3.zero;
+        }
+        
+        /// <summary>
+        /// Lerps this transforms position to a new position over the given time.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="position"></param>
+        /// <param name="duration"></param>
+        /// <returns></returns>
+        public static IEnumerator MoveToPosition(this Transform transform, Vector3 position, float duration)
+        {
+            var currentPos = transform.position;
+            var t = 0f;
+            while(t < 1)
+            {
+                t += Time.deltaTime / duration;
+                transform.position = Vector3.Lerp(currentPos, position, t);
+                yield return null;
+            }
+        }
+    }
+}
