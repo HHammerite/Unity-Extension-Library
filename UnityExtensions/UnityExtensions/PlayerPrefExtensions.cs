8224076f030c8d3b751f83e9405a871310405d0f
Harry Collin <harry.archie.collin@gmail.com>
2018-02-17 16:44:07 +0000

    Player Prefs Extension

---
+++ UnityExtensions/UnityExtensions/PlayerPrefExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public static class PlayerPrefExtensions
+{
+    public static bool GetBool (this PlayerPrefs playerPrefs, string key)
+    {
+        return PlayerPrefs.GetInt (key) == 1;
+    }
+ 
+    public static void SetBool (this PlayerPrefs playerPrefs, string key, bool state)
+    {
+        PlayerPrefs.SetInt (key, state ? 1 : 0);
+    }
+}
